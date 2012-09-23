ed15c8b7680373929ed3f257de9c761e05e17191
Simon Cropp <simon.cropp@gmail.com>
2012-09-23 16:40:46 +1000

    fix version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,4 +2,4 @@
 [assembly: AssemblyTitle("EmptyConstructor")]
 [assembly: AssemblyProduct("EmptyConstructor")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
