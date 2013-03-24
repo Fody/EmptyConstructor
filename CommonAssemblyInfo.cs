e2f09e6451e8d38c2a3670d5724ad9653304404e
Simon Cropp <simon.cropp@gmail.com>
2013-03-24 17:26:37 +1100

    version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("EmptyConstructor")]
 [assembly: AssemblyProduct("EmptyConstructor")]
-[assembly: AssemblyVersion("0.4.0.0")]
-[assembly: AssemblyFileVersion("0.4.0.0")]
+[assembly: AssemblyVersion("0.4.1.0")]
+[assembly: AssemblyFileVersion("0.4.1.0")]
