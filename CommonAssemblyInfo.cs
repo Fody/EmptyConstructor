871183b8f9e6bcfd52fc66b9d2df7080039d78d6
Simon Cropp <simon.cropp@gmail.com>
2013-07-07 20:02:24 +1000

    support for TFS checkout

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("EmptyConstructor")]
 [assembly: AssemblyProduct("EmptyConstructor")]
-[assembly: AssemblyVersion("0.4.1.1")]
-[assembly: AssemblyFileVersion("0.4.1.1")]
+[assembly: AssemblyVersion("0.4.2.0")]
+[assembly: AssemblyFileVersion("0.4.2.0")]
