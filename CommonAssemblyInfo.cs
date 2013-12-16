e223fa34b10cbe6bac7680b5cd0862b62141511d
Simon Cropp <simon.cropp@gmail.com>
2013-12-16 20:34:08 +1100

    make nuget dev time

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("EmptyConstructor")]
 [assembly: AssemblyProduct("EmptyConstructor")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
