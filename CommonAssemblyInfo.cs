a6e5fc4358f1d605901cf4da2ab4687f54297744
Simon Cropp <simon.cropp@gmail.com>
2015-08-08 12:23:10 +1000

    roll back xdt

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("EmptyConstructor")]
 [assembly: AssemblyProduct("EmptyConstructor")]
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyFileVersion("1.1.3")]
