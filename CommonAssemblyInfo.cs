d1316b8666ef33596344dda351ed45f810267aa5
Simon Cropp <simon.cropp@gmail.com>
2017-04-22 22:04:18 +1000

    fix version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 [assembly: AssemblyTitle("EmptyConstructor")]
 [assembly: AssemblyProduct("EmptyConstructor")]
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
