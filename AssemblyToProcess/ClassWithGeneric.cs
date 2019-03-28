fa6b831ef7a7f3da0e64e9acc9814e4c2afbd7b5
Bruno Lorenz Juchli <bruno.lorenz.juchli@erowa.com>
2019-03-28 11:18:24 +0100

    added missing integration tests for generics

--- AssemblyToProcess/ClassWithGeneric.cs
+++ AssemblyToProcess/ClassWithGeneric.cs
@@ -1,11 +1,32 @@
 public class ClassWithGeneric<T>
 {
+    protected ClassWithGeneric(T x)
+    {
+    }
 }
 
 public class ClassInheritWithGeneric : ClassWithGeneric<string>
 {
-// ReSharper disable once UnusedParameter.Local
+    // ReSharper disable once UnusedParameter.Local
     public ClassInheritWithGeneric(string x)
+    : base(x)
+    {
+    }
+}
+
+public class ClassWithGenericInheritWithGeneric<T> : ClassWithGeneric<T>
+{
+    protected ClassWithGenericInheritWithGeneric(T x)
+    : base(x)
+    {
+
+    }
+}
+
+public class ClassInheritWithGenericInheritWithGeneric : ClassWithGenericInheritWithGeneric<string>
+{
+    public ClassInheritWithGenericInheritWithGeneric(string x)
+    : base(x)
     {
     }
 }
